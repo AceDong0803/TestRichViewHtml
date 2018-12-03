3d98358f2bcbd07bad6960dcf5d54371b1e71f93
AceDong0803 <1165379222@163.com>
2018-12-03 20:00:02 +0800

    Add files via upload
    提交测试页面代码

---
+++ new_api_test5_files/mraid.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="/res/css/dandelion.css" media="screen" />
+<title>错误页面</title><script tpye='text/javascript' src='https://js.aq.qq.com/js/aq_common.js'></script>
+</head>
+<body>
+<div id="da-wrapper" class="fluid">
+<div id="da-content">
+<div class="da-container clearfix">
+<div id="da-error-wrapper">
+<div id="da-error-pin"></div>
+<div id="da-error-code">
+404 <br><br>Not Found </div>
+<h1 class="da-error-heading">哎哟喂！页面让狗狗叼走了！</h1>
+<h3>嗯……相信我，有页面你就能看了~</h3>
+<p>大家可以到狗狗没有叼过的地方看看！ <a href="http://x.addev.com/monitor/locationsegment#  ">回首页</a>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
